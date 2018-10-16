527ff1c2b328e1316cc5ca72d99476915213d384
Avi <av.deutsch@gmail.com>
2018-10-16 15:14:03 +0300

    added style

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ import AddProjectDescription from './pages/addProjectDescription.js';
 import AttechDrow from './pages/attechDrow.js';
 import ManagmentTools from './pages/managmentTools.js';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
-import {pageLinkes} from './linkes'
-
-
+import { pageLinkes } from './linkes'
+import { Row, Col } from 'reactstrap'
 
 class NavBar extends Component {
   render() {
     return (
-      <div className="topnav">
-        <Link to={pageLinkes.mangementTools} >Managment Tools</Link>
+      <div className="navBar">
+        <Link className to={pageLinkes.mangementTools} >Managment Tools</Link>
         <Link to={pageLinkes.projectDescreption} >Add Project Discraption</Link>
         <Link to={pageLinkes.actors}>Add Actors</Link>
         <Link to={pageLinkes.userStory}>Add User Story</Link>
@@ -34,7 +33,7 @@ class MainScreen extends Component {
     return (
       <div className='mainScreen'>
         <Route exact path={pageLinkes.mangementTools} component={ManagmentTools} />
-        <Route path='/scoping' component={NavBar} />
+        {/* <Route path='/scoping' component={NavBar} /> */}
         <Route path={pageLinkes.projectDescreption} component={AddProjectDescription} />
         <Route path={pageLinkes.actors} component={AddActors} />
         <Route path={pageLinkes.userStory} component={AddUserStories} />
@@ -56,14 +55,16 @@ class App extends Component {
     this.defineState();
     return (
       <BrowserRouter >
-        <div className='container'>
-          {/* <NavBar/> */}
-          <MainScreen/>
+        <div className='maincContainer'>
+          <Row className='header'>SCOPER</Row>
+          <Row className='articleFirst'> <NavBar /></Row>
+          <Row className='articleSecond'> <MainScreen /></Row>
+          <Row className='footer'><footer>create by COD Team ☺</footer></Row>
         </div>
       </BrowserRouter>
     );
   }
 }
-  
+
 
 export default connect(store => store)(App);
