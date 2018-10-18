cff5053649811c02674550e424fd86d935266f1b
Student <stu@ravtechj.local>
2018-10-18 13:43:27 +0300

    added new Rich Editor

--- src/pages/addProjectDescription.js
+++ src/pages/addProjectDescription.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import '../App.css';
 import { connect } from 'react-redux'
 import store from '../store/store';
+import RichEditor from '../richEditor/richEditor.js'
+import '../richEditor/richEditor.css';
 
 class AddProjectDescription extends Component {
   constructor(props) {
@@ -17,14 +19,13 @@ class AddProjectDescription extends Component {
   render() {
     return (
       <div className='formContainer description' >
-        <textarea className="projectDescreptionInput" placeholder='Describe the project' value={this.state.projectDescription}
+        {/* <textarea className="projectDescreptionInput" placeholder='Describe the project' value={this.state.projectDescription}
          onChange={e => { this.setState({ projectDescription: e.target.value })}}  disabled={this.state.editMode ? null : "disabled"}></textarea>
-         {/* {this.state.projectDescription === '' ? null : this.setState({editMode: false})} */}
          {this.state.editMode ? null : <div className='icon btn_edit' onClick={()=>this.setState({editMode: true})}>✎</div>}
          {this.state.editMode ? <button className="saveBtn" onClick={() => {
           this.setState({editMode: false})
-          store.dispatch({ type: 'PROJECT_DESCREPTION', payload: this.state.projectDescription })}}>Save</button> : null}
-      
+          store.dispatch({ type: 'PROJECT_DESCREPTION', payload: this.state.projectDescription })}}>Save</button> : null} */}
+          <RichEditor/>
         
       </div>
     );
